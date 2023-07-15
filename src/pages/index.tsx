3b1c88575c6f5057e2b2fe7ab60342c89bbfcf6e
Will Schiller <will.schiller@protonmail.com>
2023-07-15 16:41:09 +0100

    moon

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import {Image, Stack} from '@chakra-ui/react';
+
+function Page() {
+
+    return (
+      <>
+        <Stack>
+      <Image src="/pepe-planet.gif" alt="Segun Adebayo" />
+      </Stack>
+      </>
+    )
+  }
+
+export default Page
+
+
+
+
